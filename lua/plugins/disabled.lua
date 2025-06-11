365036dbb3b6b396deb4ebe96573cf9e691335b7
Samuel Boyden <sam@stboyden.com>
2025-06-11 21:25:12 +0100

    refactor: disable copilot

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    enabled = false,
+    opts = {
+      suggestions = {
+        enabled = false,
+      },
+      panel = {
+        enabled = false,
+      },
+    },
+  },
+}
