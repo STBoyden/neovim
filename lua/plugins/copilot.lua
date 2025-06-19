97bbe4869cfaa05ef927fb32fef6f1c13e8e0d5b
Samuel Boyden <sam@stboyden.com>
2025-06-19 13:20:24 +0100

    feat: re-add copilot, disable auto-trigger

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+
+    opts = {
+      suggestion = {
+        auto_trigger = false,
+        hide_during_completion = true,
+        trigger_on_accept = true,
+      },
+    },
+  },
+}
