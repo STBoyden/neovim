d032caaa8c0284325a9d85f96a1b6d54812c802c
Samuel Boyden <sam@stboyden.com>
2025-06-21 21:54:15 +0100

    feat: make theme background transparent

--- lua/plugins/github-nvim-theme.lua
+++ lua/plugins/github-nvim-theme.lua
@@ -5,7 +5,16 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("github-theme").setup({})
+      local opts = {
+        transparent = true,
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        },
+      }
+
+      require("github-theme").setup({ options = opts })
 
       vim.cmd("colorscheme github_dark_default")
     end,
