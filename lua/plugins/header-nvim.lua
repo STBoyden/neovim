de87dc0c06268b3565b779a51f7bf586b25ca208
Samuel Boyden <sam@stboyden.com>
2025-08-10 15:31:35 +0100

    refactor: instead of hardcoding year, get it automatically from os pkg

--- lua/plugins/header-nvim.lua
+++ lua/plugins/header-nvim.lua
@@ -14,7 +14,7 @@ return {
         author = "Samuel Boyden",
         date_created_fmt = "%Y-%m-%d",
         date_modified_fmt = "%Y-%m-%d",
-        copyright_text = "Copyright (c) 2025 Samuel Boyden. All rights reserved.",
+        copyright_text = "Copyright (c) " .. os.date("%Y") .. " Samuel Boyden. All rights reserved.",
         use_block_header = false,
         -- license_from_file = true,
       })
