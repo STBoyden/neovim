77f6c0eaf84ec20ef1673e5a8d3c0d9153708817
Samuel Boyden <sboyden@certosoftware.com>
2025-08-12 13:00:00 +0100

    feat: add swift to conform, add xcodebuild plugin

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,31 +1,30 @@
 -- File name: conform.lua
 -- Author: Samuel Boyden
 -- Date created: 2025-06-11
--- Date modified: 2025-08-10
+-- Date modified: 2025-08-12
 -- ------
 -- Copyright (c) 2025 Samuel Boyden. All rights reserved.
 
 return {
   {
     "stevearc/conform.nvim",
-    opts = function()
-      local opts = {
-        formatters_by_ft = {
-          go = { "golangci-lint" },
-          lua = { "stylua" },
-          sh = { "shfmt" },
-          templ = { "templ" },
-          rust = { "rustfmt" },
-        },
+    opts = {
+      formatters_by_ft = {
+        go = { "golangci-lint" },
+        lua = { "stylua" },
+        sh = { "shfmt" },
+        templ = { "templ" },
+        rust = { "rustfmt" },
+        swift = { "swift_format" },
+      },
 
-        formatters = {
-          rustfmt = {
-            prepend_args = { "+nightly" },
-          },
+      formatters = {
+        rustfmt = {
+          prepend_args = { "+nightly" },
         },
-      }
+      },
 
-      return opts
-    end,
+      log_level = vim.log.levels.ERROR,
+    },
   },
 }
---
+++ lua/plugins/xcodebuild-nvim.lua
@@ -0,0 +1,27 @@
+-- File name: xcodebuild-nvim.lua
+-- Author: Samuel Boyden
+-- Date created: 2025-08-12
+-- Date modified: 2025-08-12
+-- ------
+-- Copyright (c) 2025 Samuel Boyden. All rights reserved.
+
+local plugin = {
+  "wojciech-kulik/xcodebuild.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    -- "folke/snacks.nvim", -- (optional) to show previews
+    -- "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
+  },
+  config = function()
+    require("xcodebuild").setup({
+      -- put some options here or leave it empty to use default settings
+    })
+  end,
+}
+
+if jit.os == "OSX" then
+  return plugin
+else
+  return {}
+end
