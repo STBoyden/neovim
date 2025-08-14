ef1dda0a6e45dc3ea64250de953191f709c81dfe
Samuel Boyden <sboyden@certosoftware.com>
2025-08-14 11:54:00 +0100

    fix: update to use standard sdk path for macos as well as linux

--- lua/plugins/android-nvim.lua
+++ lua/plugins/android-nvim.lua
@@ -1,14 +1,18 @@
 -- File name: android-nvim.lua
 -- Author: Samuel Boyden
 -- Date created: 2025-08-12
--- Date modified: 2025-08-12
+-- Date modified: 2025-08-14
 -- ------
 -- Copyright (c) 2025 Samuel Boyden. All rights reserved.
 
 return {
   "ariedov/android-nvim",
   config = function()
-    vim.g.android_sdk = "~/Android/Sdk"
+    if jit.os == "OSX" then
+      vim.g.android_sdk = "~/Library/Android/sdk"
+    elseif jit.os == "Linux" then
+      vim.g.android_sdk = "~/Android/Sdk"
+    end
 
     require("android-nvim").setup()
   end,
